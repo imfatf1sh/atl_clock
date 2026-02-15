a0b42e94176632186da3852c60aabc21cd99c169
imfatf1sh <260980154+imfatf1sh@users.noreply.github.com>
2026-02-15 09:34:31 +0900

    remove auto-generated template code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,26 +12,15 @@ android {
         applicationId = "com.android.deskclock"
         minSdk = 19
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
-
-dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
 }
