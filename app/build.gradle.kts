5811889b6f55c8927cb4e9968a730b15ad0f05b8
imfatf1sh <260980154+imfatf1sh@users.noreply.github.com>
2026-02-15 09:27:16 +0900

    remove auto-generated test code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -36,7 +34,4 @@ android {
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
