f023384315c5dd73196a2bf9fb668a0adcb7129f
imfatf1sh <260980154+imfatf1sh@users.noreply.github.com>
2026-02-15 22:00:09 +0900

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,9 @@ android {
         applicationId = "com.android.deskclock"
         minSdk = 19
         targetSdk = 25
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -23,4 +26,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.1.0-rc01")
+    implementation("androidx.core:core:1.2.0-alpha02")
+    implementation("androidx.vectordrawable:vectordrawable:1.1.0-beta01")
+    implementation("androidx.appcompat:appcompat:1.1.0-beta01")
+    implementation("com.google.android.material:material:1.1.0-alpha05")
+    implementation("androidx.legacy:legacy-support-v13:1.0.0")
+    implementation("androidx.preference:preference:1.1.0-beta01")
+    implementation("androidx.gridlayout:gridlayout:1.1.0-alpha01")
+    implementation("androidx.percentlayout:percentlayout:1.0.0")
 }
